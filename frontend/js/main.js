3c159bc8a959e4158d3a5d190d9a10f87c5e5597
no-ql8 <ok240036@oks.iput.ac.jp>
2026-07-03 14:34:56 +0900

    ダミーサーバでテスト済

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,9 +1,9 @@
-// サーバ接続時堪忍事項
+// サーバ接続時確認事項
 // ・URLとポート番号が正しいか
 // ・変数名がサーバ側と一致しているか
 
 
-const API_URL = 'http://localhost:3000/api/status'; // 仮のサーバURL
+const API_URL = 'http://10.77.96.140:5000/api/status/1'; // サーバURL
 
 async function updateSeatStatus() {
     try {
@@ -11,20 +11,15 @@ async function updateSeatStatus() {
         const data = await response.json(); 
 
         // ストアID（表示はさせないかも？）
-        document.getElementById('store_id').innerText = data.store_id;
+        // document.getElementById('store_id').innerText = data.store_id;
         
         // 残席数を表示
         document.getElementById('remains-seats').innerText = `${data.remaining_seats}席`;
         
-        // 最終更新時刻を表示（Tを半角スペースに置換）-- ★1分間隔更新
+        // 最終更新時刻を表示（Tを半角スペースに置換）
         document.getElementById('updated-at').innerText = data.updated_at.replace('T', ' ');
         
-        // 詳細更新時刻（カウンター・テーブル更新時刻）を表示 -- ★5分間隔更新
-        // （拡張機能のため、仮作成）
-        // （実装の場合、API側にも追加してもらう）
-        document.getElementById('details-updated-at').innerText = data.details_updated_at.replace('T', ' ');
-
-
+        
         // データ鮮度フラグを判定(is_stale: true = 古い / false = 新しい)
         const freshnessElem = document.getElementById('freshness');
         if (data.is_stale) {
@@ -40,6 +35,8 @@ async function updateSeatStatus() {
             // 拡張機能のデータがある場合
             document.getElementById('counter-status').innerText = `${data.counter_vacant} / ${data.counter_total}席空き`;
             document.getElementById('table-status').innerText = `${data.table_vacant} / ${data.table_total}卓空き`;
+            document.getElementById('counter-updated-at').innerText = data.updated_at.replace('T', ' ');
+            document.getElementById('table-updated-at').innerText = data.updated_at.replace('T', ' ');
         } else {
             // 最小構成の場合（フィールドがない、または null のとき）
             document.getElementById('counter-status').innerText = "未対応";
